a2f94410a9b3e74f6fb2b9d4a035aadeeaa5955c
Will Smith <wsmith324@Wills-MacBook-Pro.local>
2022-09-29 11:33:50 -0400

    a

---
+++ src/main/resources/dbeaverScripts/wordsaway-dml.sql
@@ -0,0 +1,14 @@
+/*Data Manipulation LANGUAGE*/
+
+/*Insert*/
+INSERT INTO users (username,elo,email,games_played,games_won,is_cpu,"password",salt) VALUES
+	('Easy-Bot', 500, null, 0, 0, true, '', '');
+/*Update*/
+
+/*Delete*/
+
+/*Lock*/
+
+/*Call*/
+
+/*Explain Plan*/
